3702011bcc24a0f481970f22d11ce363acfc97b2
Zekseed <63977632+Zekseed@users.noreply.github.com>
2024-02-19 14:13:23 +0100

    Definitions and Colour Conversion

---
+++ main.py
@@ -0,0 +1,33 @@
+from webcolors import ( css3_hex_to_names, hex_to_rgb, )
+from scipy.spatial import KDTree
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+def convert_colour(colour):
+    css3_db = css3_hex_to_names
+    names = []
+    rgb_values = []
+    for color_hex, color_name in css3_db.items():
+        names.append(color_name)
+        rgb_values.append(hex_to_rgb(color_hex))
+    
+    kdt_db = KDTree(rgb_values)
+    distance, index = kdt_db.query(colour)
+    return f'closest match: {names[index]}'
+
+
+ev3 = EV3Brick()
+
+gripper_motor = Motor(Port.A)
+elbow_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
+base_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12,36])
+
+touch_sensor = TouchSensor(Port.S1)
+color_sensor = ColorSensor(Port.S2)
+
+# code goes here
